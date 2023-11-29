89b382a3e7342ba2eef1acdd3b5378562c5e519d
Shun_Tseng <shun_tseng@migocorp.com>
2023-11-29 12:32:03 +0800

    feat: finish mac version

---
+++ create-vanilla-js.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+# 詢問使用者輸入專案名稱
+read -p "Enter Project Name: " projectName
+
+# 檢查專案是否已經存在
+if [ -d "$projectName" ]; then
+  echo "Project Already Exists!"
+  exit 1
+fi
+
+# 創建專案資料夾，如果已經存在則什麼都不做
+mkdir "$projectName"
+
+# 進入專案資料夾
+cd "$projectName"
+
+# 創建 index.html, style.css, app.js 這三個檔案
+echo "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <link rel=\"stylesheet\" href=\"style.css\" />
+    <title>$projectName</title>
+  </head>
+  <body>
+    $projectName
+    <script src=\"./app.js\"></script>
+  </body>
+</html>" > index.html
+
+echo "/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+/* End of reset */
+
+/* Start of styles */" > style.css
+
+touch app.js
+
+echo "Project created successfully!"
+
+# 使用 VS Code 開啟專案
+code .
